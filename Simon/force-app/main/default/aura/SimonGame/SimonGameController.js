1e99af527101ffff1c6d6a960a2eb4c4350fa398
Kameron Fincher <38328487+kfincher@users.noreply.github.com>
2021-07-08 21:35:16 -0700

    This iteration is fully functioning. Just needs more styling
    There were many references to helpers that became necessary to get around issues with the context of the functions

--- Simon/force-app/main/default/aura/SimonGame/SimonGameController.js
+++ Simon/force-app/main/default/aura/SimonGame/SimonGameController.js
@@ -1,6 +1,6 @@
 ({
 	init : function(component, event, helper) {
-		helper.init(component);
+		helper.init(component, helper);
 	},
     startGame : function(component, event, helper) {
 		helper.startGame(component, event);
@@ -8,17 +8,20 @@
     
     // Input block that's tied to the color buttons
     userInputRed : function(component, event, helper) {
-		helper.handlePlayerInput(component, 0);
+		helper.handlePlayerInput(component, 0, helper);
 	},
     userInputBlue : function(component, event, helper) {
-		helper.handlePlayerInput(component, 1);
+		helper.handlePlayerInput(component, 1, helper);
 	},
     userInputGreen : function(component, event, helper) {
-		helper.handlePlayerInput(component, 2);
+		helper.handlePlayerInput(component, 2, helper);
 	},
     userInputYellow : function(component, event, helper) {
-		helper.handlePlayerInput(component, 3);
-	}
+		helper.handlePlayerInput(component, 3, helper);
+	},
+    gameEnd: function(component, event, helper) {
+		helper.gameEnd(component, helper);
+	},
     
     
 })
--- Simon/force-app/main/default/aura/SimonGame/SimonGameHelper.js
+++ Simon/force-app/main/default/aura/SimonGame/SimonGameHelper.js
@@ -1,10 +1,14 @@
 ({
-    // clears game 
-    init : function(component) {
+    // clears game/reinits everything
+    init : function(component, helper) {
         console.log('made it here');
         var rows = [];
         component.set("v.GeneratedList",rows);
         component.set("v.CurrIndex",0);
+        component.set("v.CurrDispIndex",0);
+        component.set("v.ButtonsNotPushable",true);
+        component.set("v.GameStarted",false);
+        
     },
     startGame : function(component, event) {
         console.log('startedGame'); 
@@ -25,52 +29,76 @@
     },
     // a timer countdown that recursively calls itself
     recursiveStartGame : function(helper, countdown, divCard, component){
-        window.setTimeout(function(){
-            divCard.innerHTML = countdown;
-            countdown--;
-            if(countdown>=0){
-                helper.recursiveStartGame(helper,countdown, divCard, component);
-            }else{
-                console.log("here");
-                divCard.innerHTML = "GO!";
-                
-                helper.incDiff(component);	
-                helper.playSequence(component);
-            }
-        },1000);
+        if(component.get("v.GameStarted")){
+            window.setTimeout(function(){
+                if(component.get("v.GameStarted")){
+                    divCard.innerHTML = countdown;
+                    countdown--;
+                    if(countdown>=0){
+                        helper.recursiveStartGame(helper,countdown, divCard, component);
+                    }else{
+                        console.log("here");
+                        divCard.innerHTML = "GO!";
+                        
+                        helper.incDiff(component);	
+                        helper.playSequence(component, helper);
+                    }
+                }
+            },1000); 
+        }
     },
     // playSequence -> flashIconUp (x times) -> flashIconDown (x times) -> playSequence
-    playSequence : function(component){
-        var rows = component.get("v.GeneratedList");
-        this.flashIconUp(component, this.returnColor(rows[0]), 100, .4, this, this.testFunc) // uses the number at the index to get the ID using the helper method
-        //component.set("v.ButtonsNotPushable",false);
+    playSequence : function(component, helper){
+        if(component.get("v.GameStarted")){
+            var rows = component.get("v.GeneratedList");
+            var index = component.get("v.CurrDispIndex");
+            console.log('nother sequence at index: '+index);
+            if(index<rows.length){
+                helper.flashIconUp(component, helper.returnColor(rows[index]), 75, .4, helper, helper.playSequence) // uses the number at the index to get the ID using the helper method
+            }else{
+                console.log('made it here 2');
+                helper.testFunc(component);
+            }
+        }
     },
     testFunc : function(component){
         console.log('works');
         component.set("v.ButtonsNotPushable",false);
-
     },
     incDiff : function(component){
-        var rows = component.get("v.GeneratedList");
-        rows.push(Math.floor(Math.random()*4));
-        
-        component.set("v.GeneratedList",rows);
-        console.log(rows);
-    },
-    handlePlayerInput : function(component, userInput){
         if(component.get("v.GameStarted")){
-            console.log(this.returnColor(userInput));
+            var rows = component.get("v.GeneratedList");
+            rows.push(Math.floor(Math.random()*4));
+            
+            component.set("v.GeneratedList",rows);
+            console.log(rows);
+        }
+    },
+    // plays animation
+    handlePlayerInput : function(component, userInput, helper){
+        helper.flashIconUp(component, helper.returnColor(userInput), 20, .4, helper, helper.handlePlayerInputPart2, userInput)
+    },
+    // handles logic to check for player input
+    handlePlayerInputPart2 : function(component, helper, userInput){
+        if(component.get("v.GameStarted")){
+            console.log(helper.returnColor(userInput));
             
             var index = component.get("v.CurrIndex");
             var rows = component.get("v.GeneratedList");
+            
+            
             
             if(userInput==rows[index]){
                 console.log("correct!");
                 
                 // Decides if game increase diff or resets
                 if(index+1>=rows.length){
-                    this.incDiff(component);
+                    helper.incDiff(component);
+                    component.set("v.CurrDispIndex",0);
                     component.set("v.CurrIndex",0);
+                    component.set("v.ButtonsNotPushable",true);
+                    console.log('tried to goes to second phase')
+                    helper.playSequence(component, helper);
                 }else{
                     index++;
                     component.set("v.CurrIndex",index);
@@ -78,40 +106,54 @@
                 
             }else{
                 console.log("incorrect!");
-                this.gameEnd(component)
+                helper.gameEnd(component, helper)
             }
         }
     },
-    gameEnd : function(component){
-        component.set("v.GameStarted",false);
-        this.init(component);
+    gameEnd : function(component, helper){
+        var divCard = document.getElementById("DisplayText");
+        divCard.innerHTML = "Click Start to Play!";
+        for(var i = 0;i<4;i++)
+        	document.getElementById(helper.returnColor(i)).style.setProperty('opacity', .4);
+        this.init(component, helper);
     },
     // play animation to inc opacity, and leads into flashIconDown (below)
-    flashIconUp : function(component, iconID, time, opacity, helper, func){
-        console.log('made it in');
-        window.setTimeout(function(){
-            document.getElementById(iconID).style.setProperty('opacity', opacity);
-            opacity+=.05
-            if(opacity>=1){
-                helper.flashIconDown(component, iconID, time, opacity, helper, func);
-            }else{
-                helper.flashIconUp(component, iconID, time, opacity, helper, func);
-            }
-        }, time);
+    // Used by both playSequence and handlePlayerInput
+    flashIconUp : function(component, iconID, time, opacity, helper, func, userInput){
+        if(component.get("v.GameStarted")){
+            console.log('made it in');
+            window.setTimeout(function(){
+                if(component.get("v.GameStarted")){
+                    document.getElementById(iconID).style.setProperty('opacity', opacity);
+                    opacity+=.05
+                    if(opacity>=1){
+                        helper.flashIconDown(component, iconID, time, opacity, helper, func, userInput);
+                    }else{
+                        helper.flashIconUp(component, iconID, time, opacity, helper, func, userInput);
+                    }
+                }
+            }, time);
+        }
     },    
-    flashIconDown : function(component, iconID, time, opacity, helper, func){
-        console.log('made it here')
-        window.setTimeout(function(){
-            document.getElementById(iconID).style.setProperty('opacity', opacity);
-            opacity-=.05
-            if(opacity<=.4){
-                //helper.flashIconDown(component, iconID, 500, opacity);
-                console.log('done');
-                func(component);
-            }else{
-                helper.flashIconDown(component, iconID, time, opacity, helper, func);
-            }
-        }, time);
+    flashIconDown : function(component, iconID, time, opacity, helper, func, userInput){
+        if(component.get("v.GameStarted")){
+            window.setTimeout(function(){
+                if(component.get("v.GameStarted")){
+                    document.getElementById(iconID).style.setProperty('opacity', opacity);
+                    opacity-=.05
+                    if(opacity<=.4){
+                        //helper.flashIconDown(component, iconID, 500, opacity);
+                        component.set("v.CurrDispIndex",component.get("v.CurrDispIndex") + 1);
+                        if(userInput!=null)
+                            func(component, helper, userInput); // handlePlayerInput
+                        else
+                            func(component, helper, userInput); // playSequence
+                    }else{
+                        helper.flashIconDown(component, iconID, time, opacity, helper, func, userInput);
+                    }
+                }
+            }, time);
+        }
     },
     returnColor : function(num){
         let color = "IDRed";
@@ -130,5 +172,6 @@
                 break;
         }
         return color;
+        
     }
 })
